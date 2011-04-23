8d960aed047c166129ca7f5dc5abdc4f80470ba1
Raphomet <Raphael.T.Lee@gmail.com>
2011-04-23 14:24:45 -0700

    Refactor project statuses; select status in drop down

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+
+  before_filter :require_admin, :except => [:index, :show]
+
   # GET /projects
   # GET /projects.xml
   def index
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,14 @@
 class Project < ActiveRecord::Base
   
+  STATUSES = %w( unannounced announced )
+  
   belongs_to :chapter
   
   validates_presence_of :title
   validates_presence_of :funded_at
   validates_presence_of :description
   
-  validates_inclusion_of :status, :in => %w( unannounced announced ), :allow_nil => false
+  validates_inclusion_of :status, :in => STATUSES, :allow_nil => false
   
   named_scope :unannounced, :conditions => { :status => 'unannounced' }
   named_scope :announced, :conditions => { :status => 'announced' }
