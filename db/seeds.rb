c7250fc673695a8916f4b1ffe6ea24178adc5bfc
Raphomet <Raphael.T.Lee@gmail.com>
2011-04-23 12:45:08 -0700

    Make seed user an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@
 chapter = Chapter.create! :name => 'Atlantis', :slug => 'atlantis', :tagline => "It's hotter under de water", :body => "This is a development chapter."
 
 # Create a sample user
-User.create! :login => "Poseidon", :first_name => "Poseidon", :last_name => "Johnson", :password => "password", :password_confirmation => "password", :chapter_id => chapter.id
+user = User.new(:login => "Poseidon", :first_name => "Poseidon", :last_name => "Johnson", :password => "password", :password_confirmation => "password", :chapter_id => chapter.id, :admin => true)
+user.admin = true
+user.save
